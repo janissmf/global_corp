267bc419418c9d650d2df844f482cfaff655770d
joss <joss@eisgroup.com>
2021-05-09 19:32:27 +0300

    Init Global Corp project

---
+++ src/main/java/com/gc/GlobalCorpApplication.java
@@ -0,0 +1,12 @@
+package com.gc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class GlobalCorpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalCorpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gc/GlobalCorpApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class GlobalCorpApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
